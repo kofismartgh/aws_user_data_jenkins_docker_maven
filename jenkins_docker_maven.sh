00cfc9138d1da4e94fa3224039abc7b92de2ed48
kofismartgh <90314355+kofismartgh@users.noreply.github.com>
2024-01-26 05:35:22 +0000

    Create jenkins_docker_maven

---
+++ jenkins_docker_maven.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt update
+sudo apt install openjdk-17-jdk -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl enable jenkins.service
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword > /home/ubuntu/jenkins.txt
+sudo apt install docker-ce -y
+sudo apt install maven -y
+sudo reboot
+
+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
